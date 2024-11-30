dc7c17178e82cfc86ecf1a5653bab68456b4c1c4
amrelskhawy <amrelskhawy@gmail.com>
2024-11-30 23:27:38 +0200

    Add Components together

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,38 @@
+import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
+import localFont from 'next/font/local'
+import { clsx } from "clsx"
+import { UserCard } from "@/components/userCard";
+import Card from "@/components/Card";
+
 
 export default function Home() {
   return (
     <>
-      <div className="wrapper grid grid-cols-3 gap-4">
+      <div style={{
+        gridTemplateColumns: "280px 1fr 250px"
+      }} className="wrapper grid gap-4 ">
         <div>
-          <Image
-            src={"/logo.png"}
-            width={150}
-            height={150}
-            alt="Logo"
-          />
+          <Sidebar />
         </div>
-        <div>Home</div>
+        <div className="py-3 grid gap-3">
+
+          {/* Used the local font in header with using clsx library to dynamic the style */}
+          <h2 className={clsx("text-2xl my-10 font-ClashDisplay font-medium")}>My Profile</h2>
+
+          {/* User Card Component */}
+          <UserCard />
+          <Card>
+            <div className="p-4 grid gap-2">
+              <h3 className="font-medium text-xl mb-4">About Me</h3>
+              <p className="text-[#185D43] font-medium">I’m a Medical Ambassador +  I am dedicated to transforming healthcare access and education in underserved communities. My passion for promoting health equity drives me to bridge the gap between medical professionals and those in need. Through community outreach, education, and advocacy, I strive to empower individuals with the knowledge and resources they require for better health outcomes. Together, we can build a future where quality healthcare is a right, not a privilege.</p>
+              <p className="text-[#185D43] font-medium">
+                My experience as a Medical Ambassador spans over 10 years, during which I have worked to promote access to healthcare. I have developed health awareness programs and organized medical campaigns targeting vulnerable groups, which has helped improve health awareness and promote disease prevention
+              </p>
+            </div>
+
+          </Card>
+        </div>
         <div>additional</div>
       </div>
     </>
