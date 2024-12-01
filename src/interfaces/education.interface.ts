4391cb8f0d2c73df198e7a670b80a1fcaea27bd8
amrelskhawy <amrelskhawy@gmail.com>
2024-12-01 22:12:04 +0200

    create education interface to export

---
+++ src/interfaces/education.interface.ts
@@ -0,0 +1,9 @@
+export interface IEducation {
+    university: string,
+    universityImage: string,
+    degree: string,
+    start_year: number,
+    end_year: number,
+    description?: string
+  }
+  
